2d1d8588dafd843d9f8c07f80de3b4ba2d299574
Colin McLear <mclear@fastmail.com>
2024-12-09 07:25:50 -0500

    Refactor attributes w/base dialog

--- scripts/views/dialogs/attribute-dialog.js
+++ scripts/views/dialogs/attribute-dialog.js
@@ -1,18 +1,31 @@
-import { DIALOG_DEFAULTS } from '../../config/defaults.js';
-import { handleValidationError } from '../../config/utils.js';
-import { calculateSecondaryAttributes, validateAttributeValue, ATTRIBUTES } from '../../config/attributes.js';
+// scripts/views/dialogs/attribute-dialog.js
 
-export class AttributeDialog extends FormApplication {
+import { BaseDialog } from './base-dialog.js';
+import { calculateSecondaryAttributes, validateAttributeValue, ATTRIBUTES } from '../../config/attributes.js';
+import { PathDialog } from './path-dialog.js';
+
+export class AttributeDialog extends BaseDialog {
   constructor(data = {}, options = {}) {
-    super(data, options);
-    this.characterData = data.characterData || {
-      name: "",
-      attributes: {
+    super(data, {
+      ...options,
+      nextDialogClass: PathDialog,
+      validationRules: [
+        (data) => ({
+          valid: !Object.values(data.attributes.primary).some(v => v === 0),
+          error: "Please set all attributes before continuing"
+        })
+      ]
+    });
+
+    // Initialize character data if not present
+    if (!this.characterData.attributes) {
+      this.characterData.attributes = {
         primary: { vgr: 0, grc: 0, ins: 0, prs: 0 },
         secondary: { hlt: 0, wds: 0, grt: 0, poi: 0 }
-      }
-    };
+      };
+    }
 
+    // Bind methods
     this._onRollAttributes = this._onRollAttributes.bind(this);
     this._onUseArray = this._onUseArray.bind(this);
     this._onAttributeChange = this._onAttributeChange.bind(this);
@@ -21,10 +34,9 @@ export class AttributeDialog extends FormApplication {
 
   static get defaultOptions() {
     return foundry.utils.mergeObject(super.defaultOptions, {
-      ...DIALOG_DEFAULTS,
       template: "modules/vestige-character-creator/templates/dialogs/attributes.html",
       title: "Character Attributes",
-      classes: [...DIALOG_DEFAULTS.classes, "attribute-dialog"]
+      classes: ["vestige", "dialog", "attribute-dialog"]
     });
   }
 
@@ -40,22 +52,26 @@ export class AttributeDialog extends FormApplication {
 
   activateListeners(html) {
     super.activateListeners(html);
-    html.find('button[name="roll"]').click(this._onRollAttributes.bind(this));
-    html.find('button[name="array"]').click(this._onUseArray.bind(this));
-    html.find('input[type="number"]').change(this._onAttributeChange.bind(this));
-    html.find('input[name="name"]').change(this._onNameChange.bind(this));
+
+    html.find('button[name="roll"]').click(this._onRollAttributes);
+    html.find('button[name="array"]').click(this._onUseArray);
+    html.find('input[type="number"]').change(this._onAttributeChange);
+    html.find('input[name="name"]').change(this._onNameChange);
   }
 
   async _onRollAttributes(event) {
     event.preventDefault();
-    for (const key of Object.keys(this.characterData.attributes.primary)) {
-      // Create and evaluate the roll synchronously in separate steps
-      const roll = new Roll("4d10+30");
-      await roll.evaluate(); // Use await with evaluate() instead of evaluateSync()
-      this.characterData.attributes.primary[key] = roll.total;
-    }
-    this._updateSecondaryAttributes();
-    this.render();
+
+    await this.handleAsyncOperation(async () => {
+      for (const key of Object.keys(this.characterData.attributes.primary)) {
+        const roll = new Roll("4d10+30");
+        await roll.evaluate();
+        this.characterData.attributes.primary[key] = roll.total;
+      }
+      this._updateSecondaryAttributes();
+      this.saveState();
+      this.render();
+    }, "Error rolling attributes");
   }
 
   _onAttributeChange(event) {
@@ -64,14 +80,15 @@ export class AttributeDialog extends FormApplication {
     const value = validateAttributeValue(input.value);
     this.characterData.attributes.primary[attr] = value;
     this._updateSecondaryAttributes();
+    this.saveState();
   }
 
   async _onUseArray(event) {
     event.preventDefault();
 
     const content = `
-    <form>
-      ${Object.entries(ATTRIBUTES.primary).map(([key, attr]) => `
+      <form>
+        ${Object.entries(ATTRIBUTES.primary).map(([key, attr]) => `
           <div class="form-group">
           <label>${attr.name} (${key.toUpperCase()})</label>
           <select name="${key}" required>
@@ -82,8 +99,8 @@ export class AttributeDialog extends FormApplication {
     </select>
       </div>
       `).join('')}
-    </form>
-  `;
+      </form>
+    `;
 
     const dialog = new Dialog({
       title: "Assign Attribute Values",
@@ -91,25 +108,31 @@ export class AttributeDialog extends FormApplication {
       buttons: {
         confirm: {
           label: "Confirm",
-          callback: (html) => {
-            const selections = {};
-            const usedValues = [];
+          callback: async (html) => {
+            const result = await this.handleAsyncOperation(async () => {
+              const selections = {};
+              const usedValues = [];
 
-            // Collect and validate selections
-            for (const key of Object.keys(ATTRIBUTES.primary)) {
-              const value = parseInt(html.find(`select[name="${key}"]`).val());
-              if (!value) return ui.notifications.error("All attributes must be assigned a value");
-              if (value !== 50 && usedValues.includes(value)) {
-                return ui.notifications.error("Each value (except 45) can only be used once");
+              for (const key of Object.keys(ATTRIBUTES.primary)) {
+                const value = parseInt(html.find(`select[name="${key}"]`).val());
+                if (!value) {
+                  throw new Error("All attributes must be assigned a value");
+                }
+                if (value !== 50 && usedValues.includes(value)) {
+                  throw new Error("Each value (except 50) can only be used once");
+                }
+                selections[key] = value;
+                usedValues.push(value);
               }
-              selections[key] = value;
-              usedValues.push(value);
-            }
 
-            // Apply valid selections
-            Object.assign(this.characterData.attributes.primary, selections);
-            this._updateSecondaryAttributes();
-            this.render();
+              Object.assign(this.characterData.attributes.primary, selections);
+              this._updateSecondaryAttributes();
+              await this.saveState();
+              this.render();
+              return true;
+            }, "Error assigning attributes");
+
+            return result;
           }
         },
         cancel: {
@@ -124,29 +147,17 @@ export class AttributeDialog extends FormApplication {
 
   _onNameChange(event) {
     this.characterData.name = event.currentTarget.value.trim();
+    this.saveState();
   }
 
   _updateSecondaryAttributes() {
-    this.characterData.attributes.secondary = calculateSecondaryAttributes(this.characterData.attributes.primary);
+    this.characterData.attributes.secondary = calculateSecondaryAttributes(
+      this.characterData.attributes.primary
+    );
   }
 
   async _updateObject(event, formData) {
-    // Validate attributes have been set
-    const hasZeroAttributes = Object.values(this.characterData.attributes.primary)
-          .some(value => value === 0);
-    if (hasZeroAttributes) {
-      return handleValidationError("Please set all attributes before continuing");
-    }
-
-    // Transition to PathDialog
-    const PathDialog = (await import('./path-dialog.js')).PathDialog;
-    new PathDialog(this.characterData, {
-      previousDialog: this
-    }).render(true);
-
-    // Close this dialog
-    this.close();
-    return true;
+    // Base class handles validation and navigation
+    return super._updateObject(event, formData);
   }
-
 }
